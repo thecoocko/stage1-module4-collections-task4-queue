254a515223ec3b9969b68607a6be759f2b4a6c21
thecoocko <butenkonaddezhda@gmail.com>
2023-08-01 18:36:28 +0200

    1/3

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,21 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
+/**
+ * 1..5
+ *
+ * On the round table stands numberOfDishes dishes numbered in ascending order from 1 to numberOfDishes.
+ * A man wants to try all the dishes according to the following rule:
+ *
+ * He will eat every everyDishNumberToEat dish until he has eaten everything.
+ * Implement the program which gets number of dishes as parameter numberOfDishes,
+ * dish number to eat as parameter everyDishNumberToEat and returns a List<Integer>
+ *     of dish numbers in which all dishes will be eaten.
+ * */
 public class DishOrderDeterminer {
+
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,19 @@
 package com.epam.collections.queue;
 
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
+/**Implement the program which gets two List<String>
+ * and merged them into PriorityQueue<String> alphabetically in reverse order.*/
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        List<String> sorted = new LinkedList<>();
+        sorted.addAll(firstList);
+        sorted.addAll(secondList);
+        sorted.sort((a,b)->String.CASE_INSENSITIVE_ORDER.compare(b,a));
+        priorityQueue.addAll(sorted);
+
+        return  priorityQueue;
+
     }
 }
