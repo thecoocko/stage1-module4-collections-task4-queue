88e9b528e311186b7f51e2ec63a68bf14e3f1a7d
thecoocko <butenkonaddezhda@gmail.com>
2023-08-08 21:06:01 +0200

    only 1 task is done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,11 +1,48 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 import java.util.Queue;
-
+/**
+ * Two players have two Queue<Integer> of cards with the same even sizes(>=2).
+ * They want to combine all the cards together into an ArrayDeque<Integer> according to the following rule.
+ *
+ * First, both take turns adding two cards to the ArrayDeque<Integer>.
+ * Then, with each move, the player:
+ * takes the top card from the ArrayDeque<Integer> and adds this card to the end of his Queue<Integer>
+ * then puts two cards from the beginning of his Queue<Integer> into the ArrayDeque<Integer>.
+ * The moves go until the both players' Queue<Integer> of cards are not empty.
+ *
+ * Implement the program which gets two Queue<Integer> and creates ArrayDeque<Integer>
+ *     in accordance with the rules of the game.
+ * */
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        return new ArrayDeque<Integer>();
+        ArrayDeque<Integer> combinedDeque = new ArrayDeque<>();
+        boolean player1Turn = true;
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            if (player1Turn) {
+                combinedDeque.add(firstQueue.poll());
+                combinedDeque.add(secondQueue.poll());
+                player1Turn = false;
+            } else {
+                combinedDeque.add(secondQueue.poll());
+                combinedDeque.add(firstQueue.poll());
+                player1Turn = true;
+            }
+        }
+
+        while (!firstQueue.isEmpty()) {
+            combinedDeque.add(firstQueue.poll());
+        }
+
+        while (!secondQueue.isEmpty()) {
+            combinedDeque.add(secondQueue.poll());
+        }
+
+        return combinedDeque;
     }
-}
+    }
+
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -6,6 +6,15 @@ import java.util.*;
 public class DishOrderDeterminer {
 
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
-        return new ArrayList<>();
+        List<Integer> eatenDishes = new ArrayList<>();
+        int currentDish = 1;
+        for (int i = 0; i < numberOfDishes; i++) {
+            eatenDishes.add(currentDish);
+            currentDish = (currentDish + everyDishNumberToEat - 1) % numberOfDishes + 1;
+        }
+
+        return eatenDishes;
+
     }
-}
+    }
+
